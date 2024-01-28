67f24b93504405ad09b4712448d32b66c06e4a6b
Mohit D Patel <mdpatel.csecon@outlook.com>
2024-01-28 18:29:20 -0500

    removed unnecessary CPUID function that is already available in rust core

--- charlotte_core/src/arch/x86_64/cpu/cpuid/cpuid.asm
+++ charlotte_core/src/arch/x86_64/cpu/cpuid/cpuid.asm
@@ -1,18 +1,6 @@
 bits 64
 
 section .text
-global asm_cpuid
-asm_cpuid:
-        mov r10, rbx ;preserve rbx
-        mov eax, esi ;eax = leaf
-        mov ecx, edx ;ecx = subleaf
-        cpuid
-        mov [rdi], rax
-        mov [rdi+4], rbx
-        mov [rdi+8], rcx
-        mov [rdi+12], rdx
-        mov rbx, r10 ;restore rbx
-        ret
 global asm_get_vendor_string
 asm_get_vendor_string:
         mov r10, rbx ;preserve rbx
--- charlotte_core/src/arch/x86_64/cpu/cpuid/mod.rs
+++ charlotte_core/src/arch/x86_64/cpu/cpuid/mod.rs
@@ -1,4 +1,3 @@
 extern "C" {
-    pub fn asm_cpuid(output: &mut [u32; 4], eax: u32, ecx: u32);
     pub fn asm_get_vendor_string(dest: &mut [u8; 12]);
 }
