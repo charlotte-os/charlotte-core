6837a87ef6f5633a9eace2590420783e71114a35
Ethan Plant <plant.ethan@gmail.com>
2024-02-01 16:50:32 -0500

    Request a framebuffer from Limine

--- charlotte_core/src/main.rs
+++ charlotte_core/src/main.rs
@@ -8,9 +8,28 @@ use core::fmt::Write;
 
 use arch::{Api, ArchApi};
 
+// Set the limine revision to 1
+static BASE_REVISION: limine::BaseRevision = limine::BaseRevision::new(1);
+
+// Request a framebuffer from Limine
+static FRAMEBUFFER_REQUEST: limine::FramebufferRequest = limine::FramebufferRequest::new(0);
+
 #[no_mangle]
 unsafe extern "C" fn main() -> ! {
     let mut logger = ArchApi::get_logger();
+
+    // Check if we have a framebuffer, and halt if we don't
+    if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.get_response().get() {
+        if framebuffer_response.framebuffer_count < 1 {
+            ArchApi::halt();
+        }
+
+        // Get the first framebuffer's information
+        let framebuffer = &framebuffer_response.framebuffers()[0];
+
+        write!(&mut logger, "Framebuffer located at: {:p}\n", framebuffer.address.as_ptr().unwrap());
+    }
+
     write!(&mut logger, "Initializing BSP\n").unwrap();
     ArchApi::init_bsp();
     write!(&mut logger, "BSP Initialized\n").unwrap();
