3bf8704d39f6f67748ffe68f5b9f0f6ac4c77858
Mohit D Patel <mdpatel.csecon@outlook.com>
2024-03-02 21:59:56 -0500

    fixed all compiler errors and rewrote the function to combine regions

--- charlotte_core/src/arch/x86_64/memory/pmm.rs
+++ charlotte_core/src/arch/x86_64/memory/pmm.rs
@@ -2,7 +2,6 @@ use crate::bootinfo;
 
 use core::{borrow::{Borrow, BorrowMut}, cmp::PartialOrd, mem::size_of, ops::Deref};
 
-use alloc::borrow::ToOwned;
 use spin::{lazy::Lazy, mutex::Mutex};
 
 static DIRECT_MAP: Lazy<Mutex<PhysicalAddress>> = Lazy::new(|| {
@@ -10,11 +9,11 @@ static DIRECT_MAP: Lazy<Mutex<PhysicalAddress>> = Lazy::new(|| {
         bootinfo::HHDM_REQUEST
             .get_response()
             .expect("Limine failed to create a direct mapping of physical memory.")
-            .offset(),
+            .offset() as PhysicalAddress,
     )
 });
 
-type PhysicalAddress = u64;
+type PhysicalAddress = usize;
 
 struct MemoryMap {
     entries: &'static [&'static bootinfo::memory_map::Entry],
@@ -46,9 +45,9 @@ impl MemoryMap {
     pub fn iter(&self) -> core::slice::Iter<&bootinfo::memory_map::Entry> {
         self.entries.iter()
     }
-    pub fn find_best_fit(&self, size: usize) -> &mut bootinfo::memory_map::Entry {
+    pub fn find_best_fit(&self, size: usize) -> &bootinfo::memory_map::Entry {
         self.entries
-            .iter_mut()
+            .iter()
             .filter(|entry| entry.entry_type == bootinfo::memory_map::EntryType::USABLE)
             .filter(|entry| entry.length as usize >= size)
             .min_by_key(|entry| entry.length)
@@ -91,7 +90,7 @@ impl PhysicalMemoryRegion {
 
 impl PhysicalMemoryRegion {
     fn contains(&self, other: &Self) -> bool {
-        self.base <= other.base && self.base + self.size as u64 >= other.base + other.size as u64
+        self.base <= other.base && self.base + self.size >= other.base + other.size
     }
 }
 
@@ -118,9 +117,9 @@ impl PhysicalFrameAllocator {
         let region_array_size = ((memory_map.total_memory() / FRAME_SIZE)
             * size_of::<PhysicalMemoryRegion>())
             / ESTIMATED_AVG_REGION_SIZE;
-        let mut best_fit_entry = memory_map.find_best_fit(region_array_size);
-        let pfa_base = best_fit_entry.base + best_fit_entry.length - region_array_size as u64;
-        let pfa = PhysicalFrameAllocator {
+        let best_fit_entry = memory_map.find_best_fit(region_array_size);
+        let pfa_base = (best_fit_entry.base + best_fit_entry.length - region_array_size as u64) as usize;
+        let mut pfa = PhysicalFrameAllocator {
             region_array: unsafe {
                 core::slice::from_raw_parts_mut(
                     (DIRECT_MAP.lock().deref() + pfa_base) as *mut Option<PhysicalMemoryRegion>,
@@ -134,15 +133,15 @@ impl PhysicalFrameAllocator {
             .filter(|entry| entry.entry_type == bootinfo::memory_map::EntryType::USABLE)
             .for_each(|entry| {
                 pfa.add_region(PhysicalMemoryRegion {
-                    base: entry.base,
+                    base: entry.base as usize,
                     size: entry.length as usize,
                     is_available: true,
                 })
             });
+        // merge adjacent regions
+        pfa.combine_regions();
         // Sort the region array so that we can use binary search to find regions
         pfa.region_array.sort_unstable();
-        // merge adjacent regions
-        pfa.compact_regions();
 
         pfa
     }
@@ -153,25 +152,30 @@ impl PhysicalFrameAllocator {
     }
 
     /// Combines all contiguous regions of the same type into singular regions
-    fn compact_regions(&mut self) {
-        self.region_array.sort_unstable();
-        for i in 0..self.len {
-            if let Some(region) = &mut self.region_array[i] {
-                for j in (i + 1)..self.len {
-                    if let Some(next_region) = self.region_array[j] {
-                        if region.base + region.size as u64 == next_region.base {
-                            region.size += next_region.size;
-                            self.region_array[j] = None;
-                        }
-                    }
+    fn combine_regions(&mut self) {
+        let mut i = 0usize;
+        while self.region_array[i].is_some() {
+            let mut j = i + 1;
+            while self.region_array[j].is_some() {
+                // if region_array[i] and region_array[j] are contiguous and have the same availability
+                // then increase the size of region_array[i] and make region_array[j] None
+                // Using this algorithm the region array does not need to be sorted
+                if self.region_array[i].as_ref().unwrap().base + self.region_array[i].as_ref().unwrap().size
+                    == self.region_array[j].as_ref().unwrap().base
+                    && self.region_array[i].as_ref().unwrap().is_available
+                    == self.region_array[j].as_ref().unwrap().is_available
+                {
+                    self.region_array[i].as_mut().unwrap().size += self.region_array[j].as_ref().unwrap().size;
+                    self.region_array[j] = None;
                 }
+                j += 1;
             }
+            i += 1;
         }
-        self.region_array.sort_unstable();
     }
 
     /// Returns the index of the smallest region
-    fn smallest_region_index(&self) -> Result<usize, Error> {
+    fn smallest_region(&self) -> Result<(usize, &Option<PhysicalMemoryRegion>), Error> {
         self.region_array
             .iter()
             .enumerate()
@@ -186,7 +190,7 @@ impl PhysicalFrameAllocator {
             .ok_or(Error::OutOfMemory)
     }
 
-    fn largest_region_index(&self) -> Result<usize, Error> {
+    fn largest_region(&self) -> Result<(usize, &Option<PhysicalMemoryRegion>), Error> {
         self.region_array
             .iter()
             .enumerate()
@@ -215,7 +219,7 @@ impl PhysicalFrameAllocator {
             .sum()
     }
 
-    fn best_fit_region_index(&self, size: usize) -> Result<usize, Error> {
+    fn best_fit_region(&self, size: usize) -> Result<(usize, &Option<PhysicalMemoryRegion>), Error> {
         if self.available_memory() < size {
             if self.available_memory() == 0 {
                 Err(Error::OutOfMemory)
@@ -240,12 +244,12 @@ impl PhysicalFrameAllocator {
     }
 
     pub fn allocate_contiguous(&mut self, count: usize) -> Result<PhysicalMemoryRegion, Error> {
-        let alloc_region_index = self.best_fit_region_index(count * FRAME_SIZE)?;
+        let alloc_region_index = self.best_fit_region(count * FRAME_SIZE)?.0;
         // Safety: The best_fit_region_index function ensures that the region is available
         let mut alloc_region = unsafe { self.region_array[alloc_region_index].unwrap_unchecked() };
         alloc_region.size -= count * FRAME_SIZE;
         let new_region = PhysicalMemoryRegion::new(
-            alloc_region.base + alloc_region.size - count * FRAME_SIZE,
+            (alloc_region.base + alloc_region.size - count * FRAME_SIZE) as PhysicalAddress,
             count * FRAME_SIZE,
             false,
         );
@@ -256,7 +260,7 @@ impl PhysicalFrameAllocator {
 
     pub fn deallocate(&mut self, region: PhysicalMemoryRegion) -> Result<(), Error> {
         //find the containing region in the region array
-        let mut containing_region = unsafe {
+        let containing_region = 
             // Safety: The region array is filtered to ensure the region is in the array.
             // If not the appropriate error is propagated.
             self
@@ -269,16 +273,16 @@ impl PhysicalFrameAllocator {
                         false
                     }
                 })
-                .find(|region| {
-                    if region.is_some() {
-                        region.as_ref().unwrap().contains(&region)
+                .find(|r| {
+                    if r.is_some() {
+                        r.as_ref().unwrap().contains(&region)
                     } else {
                         false
                     }
                 })
                 .ok_or(Error::AllocatedRegionNotFound)?
-                .unwrap_unchecked();
-        };
+                .as_mut()
+                .unwrap();
         //deallocate the portion of the region represented by the input region
         let new_region = PhysicalMemoryRegion::new(
             region.base,
@@ -289,21 +293,21 @@ impl PhysicalFrameAllocator {
         //and adjust the containing region accordingly
         if containing_region.base == region.base {
             //if the region is at the beginning, adjust the base and size of the containing region
-            containing_region.base += region.size as u64;
+            containing_region.base += region.size;
             containing_region.size -= region.size;
-        } else if containing_region.base + containing_region.size as u64 == region.base + region.size as u64 {
+        } else if containing_region.base + containing_region.size == region.base + region.size {
             //if the region is at the end, adjust the size of the containing region
             containing_region.size -= region.size;
         } else {
             //if the region is in the middle, split the containing region into two regions one before and one after the input region
-            *containing_region = Some(PhysicalMemoryRegion {
+            *containing_region = PhysicalMemoryRegion {
                 base: containing_region.base,
                 size: region.base - containing_region.base,
                 is_available: false,
-            });
+            };
             let after_region = PhysicalMemoryRegion::new(
-                region.base + region.size as u64,
-                containing_region.base + containing_region.size as u64 - region.base + region.size as u64,
+                region.base + region.size,
+                containing_region.base + containing_region.size - region.base + region.size,
                 false,
             );
             self.add_region(after_region);
