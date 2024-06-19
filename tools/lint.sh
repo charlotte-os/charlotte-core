3087b7e3ca0823accc9a527bdf0772582c1b2697
Wrench56 <dmarkreg@gmail.com>
2024-06-18 21:21:04 -0400

    Add `clippy` check in `lint.sh`

--- tools/lint.sh
+++ tools/lint.sh
@@ -4,6 +4,6 @@ cargo fmt --check --manifest-path charlotte_core/Cargo.toml
 
 # TODO: Keep targets updated
 cargo check --target x86_64-unknown-none --manifest-path charlotte_core/Cargo.toml
-# cargo clippy --target x86_64-unknown-none --manifest-path charlotte_core/Cargo.toml
+cargo clippy --target x86_64-unknown-none --manifest-path charlotte_core/Cargo.toml
 
 grep -rn '#\[allow\(.*\)\]' ./charlotte_core/src/
