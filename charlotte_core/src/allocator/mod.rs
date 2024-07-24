6f846c6b75bb2da1eb50a9815b490141820f07e7
Mohit D Patel <mdpatel.csecon@outlook.com>
2024-07-23 23:12:28 -0400

    formatting

--- charlotte_core/src/allocator/mod.rs
+++ charlotte_core/src/allocator/mod.rs
@@ -1,11 +1,10 @@
 /// # Memory Allocator
-/// 
+///
 /// This module contains the dynamic memory allocator for the kernel. The allocator is responsible for
 /// managing the memory used by the kernel. The allocator is used to allocate memory for the kernel
 /// and to free memory that is no longer needed.
-/// 
+///
 /// The allocator is not thread-safe and instances will thus always need to be placed behind a mutex or a binary semaphore.
-
 use core::ptr::NonNull;
 
 use crate::memory::address::VirtualAddress;
@@ -18,5 +17,5 @@ struct Allocation {
 
 struct Allocator<const base: VirtualAddress, const size: usize> {
     large_allocations: Option<NonNull<Allocation>>,
-    small_allocations: Option<NonNull<Allocation>>
+    small_allocations: Option<NonNull<Allocation>>,
 }
