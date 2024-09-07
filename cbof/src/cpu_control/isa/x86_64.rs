38613d923788ac9b38f9316b344572f99761aaaa
Mohit D Patel <mdpatel.csecon@outlook.com>
2024-09-06 23:19:13 -0400

    Added inline hints to some wrapper functions.

--- cbof/src/cpu_control/isa/x86_64.rs
+++ cbof/src/cpu_control/isa/x86_64.rs
@@ -6,18 +6,19 @@ use crate::cpu_control::CpuControl;
 pub struct CpuControlX86_64;
 
 impl CpuControl for CpuControlX86_64 {
+    #[inline]
     fn enable_interrupts() {
         unsafe {
             x86_64::instructions::interrupts::enable();
         }
     }
-
+    #[inline]
     fn disable_interrupts() {
         unsafe {
             x86_64::instructions::interrupts::disable();
         }
     }
-
+    #[inline]
     fn halt() -> ! {
         loop {
             hlt();
